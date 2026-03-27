53817b1ea5b24e8ec6ba39be20e88fc2b5269f87
KCReads <107605421+KCReads@users.noreply.github.com>
2026-03-26 21:52:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const sheetURL = "YOUR_CSV_LINK"; // ← replace this
+const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRpcuB3lP6poEiXufRP7C_pdB3ZHz4WB82Zg5JmLSUg_BvjoC7xM5BDqG5PhdZOFg/pub?gid=1251597746&single=true&output=csv"; // ← replace this
 let data = [];
 
 // Load data
