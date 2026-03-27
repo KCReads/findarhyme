92b37d03bee9462a62b60f156e8b1d332eee5c08
KCReads <107605421+KCReads@users.noreply.github.com>
2026-03-27 01:15:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const favEl = document.getElementById("favoritesOnly");
 /* ======================
    LOAD CSV
 ====================== */
-Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vRpcuB3lP6poEiXufRP7C_pdB3ZHz4WB82Zg5JmLSUg_BvjoC7xM5BDqG5PhdZOFg/pub?gid=1251597746&single=true&output=csv", {
+Papa.parse("data.csv", {
   download: true,
   header: true,
   complete: function(results) {
